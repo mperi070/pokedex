f65752840f0026a017f3cce9b0716607680ded73
Melani Peries <mperies1109@yahoo.com>
2021-03-15 14:37:43 -0400

    Initial commit

---
+++ scripts.js
@@ -0,0 +1,5 @@
+let pokemonList = [
+    {name: 'Empoleon', height: 7, types: ['water', 'steel']},
+    {name: 'Luxray', height: 4, types: ['electric']},
+    {name: 'Grimmsnarl', height: 6, types: ['dark', 'fairy']}
+];
