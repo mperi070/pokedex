a2e5746558379bac1cc375377d3b0dd16869462f
Melani Peries <mperies1109@yahoo.com>
2021-04-03 19:03:45 -0400

    Added if else statement

--- scripts.js
+++ scripts.js
@@ -1,9 +1,14 @@
 let pokemonList = [
-    {name: 'Empoleon', height: 6, types: ['water', 'steel']},
     {name: 'Luxray', height: 4, types: ['electric']},
+    {name: 'Empoleon', height: 6, types: ['water', 'steel']},
     {name: 'Grimmsnarl', height: 5, types: ['dark', 'fairy']}
 ];
 
 for (let i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' );
+    if (pokemonList[i].height > 5) {
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') - Wow, that is big! ' );
+    }
+    else {
+        document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' );
+    }
 }
