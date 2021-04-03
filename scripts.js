e87cb165a73e52ce80d7f5a9ff611ad539950abb
Melani Peries <mperies1109@yahoo.com>
2021-04-03 18:52:09 -0400

    Updated for loop

--- scripts.js
+++ scripts.js
@@ -1,9 +1,9 @@
 let pokemonList = [
-    {name: 'Empoleon', height: 7, types: ['water', 'steel']},
+    {name: 'Empoleon', height: 6, types: ['water', 'steel']},
     {name: 'Luxray', height: 4, types: ['electric']},
-    {name: 'Grimmsnarl', height: 6, types: ['dark', 'fairy']}
+    {name: 'Grimmsnarl', height: 5, types: ['dark', 'fairy']}
 ];
 
 for (let i = 0; i < pokemonList.length; i++) {
-    document.write(pokemonList);
+    document.write(pokemonList[i].name + ' (height: ' + pokemonList[i].height + ') ' );
 }
