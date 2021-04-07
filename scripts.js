357fd2b017db77e608af6aef6c2152fdb6a3a7fa
Melani Peries <mperies1109@yahoo.com>
2021-04-07 19:02:09 -0400

    IIFE creation
    Wrapped pokemonList array into an IIFE and then assigned it to a new pokemonRepository variable

--- scripts.js
+++ scripts.js
@@ -1,8 +1,12 @@
-let pokemonList = [
-    {name: 'Luxray', height: 4, types: ['electric']},
-    {name: 'Empoleon', height: 6, types: ['water', 'steel']},
-    {name: 'Grimmsnarl', height: 5, types: ['dark', 'fairy']}
-];
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {name: 'Luxray', height: 4, types: ['electric']},
+        {name: 'Empoleon', height: 6, types: ['water', 'steel']},
+        {name: 'Grimmsnarl', height: 5, types: ['dark', 'fairy']}
+    ];
+}) ();
+
+
 
 // for (let i = 0; i < pokemonList.length; i++) {
 //     if (pokemonList[i].height > 5) {
