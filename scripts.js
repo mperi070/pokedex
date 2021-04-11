6052eef3c35ddf7301df3d9f9da56148c84b9585
Melani Peries <mperies1109@yahoo.com>
2021-04-11 17:34:21 -0400

    Fixed button.innerText
    Incorrectly had quotes - prevented parameter being passed

--- scripts.js
+++ scripts.js
@@ -18,7 +18,7 @@ let pokemonRepository = (function () {
         let pokemonList = document.querySelector('.pokemon-list');
         let listItem = document.createElement('li');
         let button = document.createElement('button');
-        button.innerText = 'pokemon.name';
+        button.innerText = pokemon.name;
         button.classList.add('button-class');
         listItem.appendChild(button);
         pokemonList.appendChild(listItem);
